b9d568a778109a36753be18f9eeecd766299d1f8
Colin HARRIS <col.w.harris@gmail.com>
2017-09-20 19:08:28 +0800

    initial commit

---
+++ AlamofireMocks/AlamofireMocks.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireMocks.h
+//  AlamofireMocks
+//
+//  Created by Colin HARRIS on 9/20/17.
+//  Copyright © 2017 Colin Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireMocks.
+FOUNDATION_EXPORT double AlamofireMocksVersionNumber;
+
+//! Project version string for AlamofireMocks.
+FOUNDATION_EXPORT const unsigned char AlamofireMocksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireMocks/PublicHeader.h>
+
+
